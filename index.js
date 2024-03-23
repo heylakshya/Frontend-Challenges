b48ab64cc972e1e248892f198cddaa1f21092a50
Lakshya <lakshya@Lakshyas-MBP.domain.name>
2024-03-24 04:03:51 +0530

    attempt to create index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 // function isProjectDirectory(path) {
 //     if(fs.statSync(path).isDirectory()){
